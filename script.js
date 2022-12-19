88d0b7fedbfa99bc74534a0f21ec4fe594f1489d
jespervdz <jespervdzwaag@gmail.com>
2022-12-19 11:10:27 +0100

    Improved check to see if page has a document

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ let d2lbutton = null;
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
   if (document.readyState === 'complete') {
-    if (headerButtonTray === null || d2lbutton === null) {
+    if (headerButtonTray === null || d2lbutton === null)
       setElements();
-    } else {
+    else
       setButtonVisibility();
-    }
   }
 };
 
@@ -28,6 +27,10 @@ function setElements() {
 
       if (headerButtonTrays.length > 0) {       
         headerButtonTray = headerButtonTrays[0];
+
+        // Safety check if it already exists.
+        if (headerButtonTray.getElementsByClassName('new-tab-button-extension').length > 0)
+          return;
 
         // Add the button to the tray
         headerButtonTray.insertAdjacentHTML('afterbegin', html);
@@ -57,10 +60,16 @@ function openFileInNewTab() {
   }
 }
 
-// Checks if the page has an iframe wrapped document that can be opened in a new tab
+// Checks if the page has an content-panel with a file document that can be opened in a new tab
 function pageHasDocument() {
   try {
-    return document.getElementsByTagName('d2l-iframe-wrapper-for-react')[0].getAttribute('src') != null;
+    let contentPanels = document.getElementsByClassName('content-panel');
+
+    if (contentPanels.length === 0)
+      return false;
+
+    let contentPanel = contentPanels[0];
+    return contentPanel.getAttribute('data-attr-activity-type') === 'file';
   } catch {
     return false;
   }
